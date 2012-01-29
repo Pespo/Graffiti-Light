39148ecced9fd0a72702ea63221c2effe37d3440
Tom GAY <muxisar@gmail.com>
2012-01-29 12:20:25 +0100

    First SDL loop

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@
 
 void initSDLOpenGL();
 void initFBOs();
+void resize(GLuint w, GLuint h);
+void handleUserEvent(const SDL_Event& event);
+void handleKeyEvent(const SDL_keysym& keysym, bool down);
+void animate();
+void handleEvent(const SDL_Event& event);
+void renderFrame();
+void printGlErrors();
+void loop();
+void initTimers();
+static inline Uint32 genericTimer(Uint32 interval, void* param);
 
 size_t fullScreenWidth;
 size_t fullScreenHeight;
@@ -15,6 +25,12 @@ size_t width;
 size_t height;
 size_t windowedWidth;
 size_t windowedHeight;
+static int timerId = 1;
+
+GLuint frameCount;
+uint64_t m_LastStartTime;
+uint64_t m_FrameDuration;
+SDL_TimerID animateTimer;
 
 bool bRunning;
 
@@ -33,8 +49,12 @@ int main (int argc, char** argv) {
         
     // Initialisation of SDL and creation of OpenGL context
     initSDLOpenGL();
-    
+    loop();
     return 0;
+}
+
+void initTimers() {
+    animateTimer = SDL_AddTimer(20, genericTimer, &timerId);
 }
 
 void initSDLOpenGL() {
@@ -76,6 +96,116 @@ void initSDLOpenGL() {
     glClearColor(0., 0., 0., 1.);
     
     glLineWidth(2);
+}
+
+void resize(GLuint w, GLuint h) {
+    
+    width = w;
+    height = h;
+    
+    pDrawContext = SDL_SetVideoMode(width, height, 0, videoModeFlags);
+
+    glViewport(0, 0, width, height);
+}
+
+void handleUserEvent(const SDL_Event& event) {  
+    if (event.user.code == timerId) {
+        animate();
+        renderFrame();
+        
+        printGlErrors();
+        
+        ++frameCount;
+        if (frameCount % 20 == 0) {
+            //uint64_t time = getTime();
+            //m_FrameDuration = (time - m_LastStartTime) / 20LL;
+            //m_LastStartTime = time;
+        }
+    }
+}
+
+void handleKeyEvent(const SDL_keysym& keysym, bool down) {
+    if (down) {
+        switch (keysym.sym) {
+            case SDLK_ESCAPE:
+                bRunning = false;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void animate() {
+}
+
+void handleEvent(const SDL_Event& event) {
+    switch (event.type) {
+        case SDL_USEREVENT:
+            handleUserEvent(event);
+            break;
+        case SDL_KEYDOWN:
+            handleKeyEvent(event.key.keysym, true);
+            break;
+        case SDL_KEYUP:
+            handleKeyEvent(event.key.keysym, false);
+            break;
+        case SDL_QUIT:
+            bRunning = false;
+            break;
+        default:
+            break;
+    }
+}
+
+void renderFrame() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ 
+    //m_Scene.drawObjectsOfScene();
+    
+    SDL_GL_SwapBuffers();
+}
+
+static inline Uint32 genericTimer(Uint32 interval, void* param) {
+    SDL_Event event;
+    event.type = SDL_USEREVENT;
+    event.user.code = *static_cast<int*>(param);
+    event.user.data1 = 0;
+    event.user.data2 = 0;
+    SDL_PushEvent(&event);
+    return interval;
+}
+
+void printGlErrors() {
+    GLenum error = glGetError();
+    
+    while (error != GL_NO_ERROR) {
+        cout << "!! GL Error : ";
+        if (error == GL_INVALID_ENUM)
+            cout << "GL_INVALID_ENUM" << endl;
+        if (error == GL_INVALID_VALUE)
+            cout << "GL_INVALID_VALUE" << endl;
+        if (error == GL_INVALID_OPERATION)
+            cout << "GL_INVALID_OPERATION" << endl;
+        if (error == GL_OUT_OF_MEMORY)
+            cout << "GL_OUT_OF_MEMORY" << endl;
+        
+        error = glGetError();
+        
+        cout << endl;
+    }
+}
+
+void loop() {
+    initTimers();
+    //m_LastStartTime = getTime();
+    SDL_Event event;
+    
+    while (bRunning) {
+        while (SDL_PollEvent(&event))
+            handleEvent(event); // Checks each event for types and loach corresponding actions
+        printGlErrors();
+    }
 }
 
 void initFBOs() {
